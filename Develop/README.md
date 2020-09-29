0bdb0c0e8723de0cde69c0a5a821fdff6ea215e6
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-09-29 02:30:47 -0400

    Uploads.

---
+++ Develop/README.md
@@ -0,0 +1,54 @@
+# TEst
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+Example
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+npm install
+```
+
+## Usage
+
+node index answer qs
+
+## License
+
+This project is licensed under the MIT license.
+  
+## Contributing
+
+tDN
+
+## Tests
+
+To run tests, run the following command:
+
+```
+undefined
+
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at dnickle.cet@gmail.com. You can find more of my work at [TestadeNevill](https://github.com/TestadeNevill/).
+
---
+++ Develop/index.js
@@ -0,0 +1,75 @@
+
+//Loading required Modules
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a detailed description of your project"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which open source license did you choose?",
+    choices: ["MIT", "APACHIE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?",
+    default: "npm install"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use."
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Name collaborators"
+  },
+  // {
+  //   type: "input",
+  //   name: "bagdes",
+  //   message: "List any badges"
+  // },
+
+  //rest of the questions here....
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// function to initialize program
+function init() {
+  inquirer.prompt(questions).then((inquireResponses) => {
+    writeToFile("README.md", generateMarkdown({ ...inquireResponses }));
+  })
+}
+
+// function call to initialize program
+init();
